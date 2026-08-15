a6c33080090749bc4657dd04c501ac768e34d8a1
SheyBoiCarti <sheharyarnadeem91@gmail.com>
2026-08-15 14:32:16 +0200

    Time: 0 ms (100%), Space: 47.2 MB (5.87%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        
+        int validindex=0;
+
+        for(int i =0;i<nums.Length;i++){
+            if(nums[i]!= val)
+            {
+                nums[validindex]= nums[i];
+                validindex++;
+            }
+        }
+        return validindex;
+    }   
+}

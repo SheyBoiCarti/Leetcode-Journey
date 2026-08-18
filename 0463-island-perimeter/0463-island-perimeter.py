8c2cec10a5edfbb1aa90b2981ee2d767a4bf49db
SheyBoiCarti <sheharyarnadeem91@gmail.com>
2026-08-18 21:46:23 +0200

    Time: 25 ms (87.37%), Space: 19.7 MB (44.18%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,33 +1,33 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        #loop through all the elements
-        #if we encounter one, check its left,right, up, and down
-        #if we encounter boundary, increase perimieter
-        #if we encounter 0 increase perimeter
+        # Loop through all the elements
+        # If we encounter a 1, check its left, right, up, and down
+        # If we encounter a boundary, increase the perimeter
+        # If we encounter a 0, increase the perimeter
 
         rows = len(grid)
-        cols= len(grid[0])
+        cols = len(grid[0])
 
-        perimeter=0
-        #need to check bounds(means we hit a boundary) or if going in that direction gives us an zero
+        perimeter = 0
+
+        # Check if we hit a boundary or if moving in that direction gives us a 0
         for x in range(rows):
             for y in range(cols):
-                if grid[x][y]==1:
-                    #go left
-                    if y-1 <0 or grid[x][y-1] ==0:
-                        perimeter+=1
-                    #right
-                    if y+1 >=cols or grid[x][y+1] ==0:
-                        perimeter+=1
-                    #go up 
-                    if x-1 <0 or grid[x-1][y] ==0:
-                        perimeter+=1
-                    #go down
-                    if x+1 >=rows or grid[x+1][y] ==0:
-                        perimeter+=1
+                if grid[x][y] == 1:
+                    # Go left
+                    if y - 1 < 0 or grid[x][y - 1] == 0:
+                        perimeter += 1
+
+                    # Go right
+                    if y + 1 >= cols or grid[x][y + 1] == 0:
+                        perimeter += 1
+
+                    # Go up
+                    if x - 1 < 0 or grid[x - 1][y] == 0:
+                        perimeter += 1
+
+                    # Go down
+                    if x + 1 >= rows or grid[x + 1][y] == 0:
+                        perimeter += 1
+
         return perimeter
-
-
-                    
-
-        

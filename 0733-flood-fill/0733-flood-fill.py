4f8f02fc98e6f66777dab71a41bf9b7db2c33fd3
SheyBoiCarti <sheharyarnadeem91@gmail.com>
2026-08-18 18:15:38 +0200

    Time: 0 ms (100%), Space: 19.8 MB (16.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,54 +1,31 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 
-        #start flood fill at image[sr][sc]
+        rows = len(image)
+        cols = len(image[0])
 
-        rows= len(image) 
-        cols= len(image[0])
+        old_color = image[sr][sc]
 
-        boundary_x= rows-1
-        boundary_y= cols-1
+        if old_color == color:
+            return image
 
-        old_color= image[sr][sc]
+        def floodFill(x, y):
 
-        visited= [[False] * cols for _ in range(rows)]
-        print(visited)
+            # out of bounds
+            if x < 0 or x >= rows or y < 0 or y >= cols:
+                return
 
-        def floodFill(image, x,y, boundary_x, boundary_y, color):
-            """
-            #check if we can go up
-            if x<0:
-                #cant go up anymore
-            if x > boundary_x:
-                #cant go down anymore
-            if y <0: 
-                #cant go left
-            if y> boundary_y: 
-                #cant go right
-            """
-            # out of boundary for current cell
-            if x<0 or x> boundary_x or y<0 or y > boundary_y:
-                return None
+            # wrong color or already changed
+            if image[x][y] != old_color:
+                return
 
-            #already visited cell
-            if visited[x][y]== True :
-                return None
-            
-            # color doesnt match
-            if old_color != image[x][y]:
-                return None
+            image[x][y] = color
 
-            #now we at a valid cell
+            floodFill(x - 1, y)  # up
+            floodFill(x + 1, y)  # down
+            floodFill(x, y - 1)  # left
+            floodFill(x, y + 1)  # right
 
-            image[x][y]= color
-            visited[x][y]= True
+        floodFill(sr, sc)
 
-            floodFill(image, x-1,y, boundary_x, boundary_y, color) #go up
-            floodFill(image, x+1,y, boundary_x, boundary_y, color) #go down
-            floodFill(image, x,y-1, boundary_x, boundary_y, color) #go left
-            floodFill(image, x,y+1, boundary_x, boundary_y, color) #go right
-        
-        floodFill(image, sr, sc, boundary_x, boundary_y, color)
-        print(visited)
         return image
-     

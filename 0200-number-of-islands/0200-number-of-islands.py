bbf72b23fb39809fd4f2b6dd5e660e29c745596b
SheyBoiCarti <sheharyarnadeem91@gmail.com>
2026-08-18 19:43:13 +0200

    Time: 247 ms (48.68%), Space: 21.7 MB (47.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,23 +5,25 @@ class Solution:
         rows= len(grid)
         cols= len(grid[0])
 
-        visited= [[False] * cols for _ in range(rows)]
+        #visited= [[False] * cols for _ in range(rows)]
 
         def dfs(grid, x,y):
             nonlocal numberOfIslands
             if x< 0 or x>= rows or y<0 or y>=cols:
                 return None
 
-            if visited[x][y]== True:
-                return None
+            #if visited[x][y]== True:
+                #return None
             
         
             if grid[x][y]=='0':
-                visited[x][y]= True
+                #visited[x][y]= True
                 return None
         
             #valid
-            visited[x][y]= True
+            #visited[x][y]= True
+
+            grid[x][y]= '0'
           
             dfs(grid,x+1,y) #down
             dfs(grid,x-1,y) #up
@@ -31,11 +33,12 @@ class Solution:
             
         for i in range(rows):
             for j in range (cols):
-                if grid[i][j] == '1' and visited[i][j]==False:
+                ##if grid[i][j] == '1' and visited[i][j]==False:
+                if grid[i][j] == '1':
                     numberOfIslands+=1
                 dfs(grid,i,j)
 
-        print(visited)
+        #print(visited)
 
         return numberOfIslands
             

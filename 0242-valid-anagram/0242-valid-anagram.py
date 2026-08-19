cd23db509475c7470006f211b984b327fe4274ca
SheyBoiCarti <sheharyarnadeem91@gmail.com>
2026-08-19 17:18:58 +0200

    Time: 18 ms (19.18%), Space: 19.4 MB (76.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        sorted_s = ''.join(sorted(s))
-        sorted_t= ''.join(sorted(t))
+        hashmap={}
 
-        return sorted_s == sorted_t
+        for c in s:
+            if c in hashmap:
+                hashmap[c]= hashmap[c]+1
+            else: hashmap[c]=1
+        
+        for c2 in t:
+            if c2 in hashmap:
+                if hashmap[c2] >0:
+                    hashmap[c2]= hashmap[c2]-1
+                    if hashmap[c2] ==0 :
+                        del hashmap[c2]
+            else:return False
+        
+        print(len(hashmap))
+        return len(hashmap) ==0
+
         

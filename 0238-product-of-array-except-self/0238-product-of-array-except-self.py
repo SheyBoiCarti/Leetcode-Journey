cb37160175d804e24a35fa0fd1ea59397dd5522b
SheyBoiCarti <sheharyarnadeem91@gmail.com>
2026-08-19 19:49:54 +0200

    Time: 35 ms (25.58%), Space: 27.7 MB (21.08%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,38 +3,19 @@ class Solution:
 
         Prefix_left_product = [0] * len(nums)
         Suffix_right_Product = [0] * len(nums)
-
-        Prefix_left_product[0] = nums[0]
-        Suffix_right_Product[len(nums) - 1] = nums[len(nums) - 1]
-
-        # The idea is to get the product of everything to the left
-        # and everything to the right of the current number
-        # Then multiply those together so the current number is excluded
-
         result = [0] * len(nums)
 
-        # Build up the products from left to right
-        for i in range(1, len(nums)):
-            Prefix_left_product[i] = Prefix_left_product[i - 1] * nums[i]
+        Prefix_left_product[0] = 1
+        Suffix_right_Product[len(nums) - 1] = 1
 
-        # Build up the products from right to left
+        
+        for i in range(1, len(nums)):
+            Prefix_left_product[i] = Prefix_left_product[i - 1] * nums[i - 1]
+
         for j in range(len(nums) - 2, -1, -1):
-            Suffix_right_Product[j] = Suffix_right_Product[j + 1] * nums[j]
+            Suffix_right_Product[j] = Suffix_right_Product[j + 1] * nums[j + 1]
 
         for i in range(len(nums)):
-
-            if i == 0:
-                # First number has nothing to the left
-                # so we only need the product of everything to the right
-                result[i] = Suffix_right_Product[i + 1]
-
-            elif i == len(nums) - 1:
-                # Last number has nothing to the right
-                # so we only need the product of everything to the left
-                result[i] = Prefix_left_product[i - 1]
-
-            else:
-                # Multiply everything to the left by everything to the right
-                result[i] = Prefix_left_product[i - 1] * Suffix_right_Product[i + 1]
+            result[i] = Prefix_left_product[i] * Suffix_right_Product[i]
 
         return result
